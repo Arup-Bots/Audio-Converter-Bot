d32b5e7274baeda7a4bc29e8942d14389ca78339
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2022-01-09 12:45:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pyrogram.errors import UserNotParticipant, UserBannedInChannel
 #configs 
 DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
 DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/AudioBoT/")
-FORCE_SUB_CHANNEL = int(os.environ.get("FSUB_CHANNEL", "0"))
+FSUB_CHANNEL = int(os.environ.get("FSUB_CHANNEL", ""))
 
 #strings
 START_TEXT = """
